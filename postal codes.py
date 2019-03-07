ce17bdbd189e178869b7d8cb8703204cd3a80cf9
Xuyang Han <43256411+xuyangHan@users.noreply.github.com>
2019-03-07 17:11:40 -0500

    Create postal codes.py

---
+++ postal codes.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import requests
+
+# to get codes df from the postal csv
+codes_df = pd.read_excel(r'C:\Users\admin\Documents\github\genderEquality\static\data\sample codes.xlsx')
+
+# some codes to get lat and long from google maps api
+i = 0
+lat = []
+long = []
+for code in codes_df['Codes']:
+    api_url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' \
+              + code + \
+              '&key=AIzaSyDCd0jbz9KTcTtx3QV-DafS44lsXDBAmHY'
+    response = requests.get(url=api_url)
+    data = response.json()
+    loc_lat = data['results'][0]["geometry"]['location']['lat']
+    loc_long = data['results'][0]["geometry"]['location']['lng']
+    lat.append(loc_lat)
+    long.append(loc_long)
+    i = i + 1
+    print(i)
+
+# some codes to generate a csv file with lat and long
+codes_df['lat'] = lat
+codes_df['long'] = long
+writer = pd.ExcelWriter('static/data/example.xlsx', engine = 'xlsxwriter')
+codes_df.to_excel(writer, sheet_name='sheet1')
+writer.save()
+
+
