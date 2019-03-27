843929b83b2cb1fa4519ad701962443678043e7b
Xuyang Han <43256411+xuyangHan@users.noreply.github.com>
2019-03-27 17:19:48 -0400

    Create final score.py

---
+++ final score.py
@@ -0,0 +1,21 @@
+import json
+import pandas as pd
+from shapely.geometry import shape, Point
+
+final_score = pd.read_csv('static/data/final.CSV')
+
+# open geojson
+with open('static/data/map.geojson') as json_file:
+    data = json.load(json_file)
+    print(len(data['features']))
+
+for feature in data['features']:
+    polygon = shape(feature['geometry'])
+    for index, row in final_score.iterrows():
+        p = Point(row['cent_long'], row['cent_lat'])
+        if polygon.contains(p):
+            feature.update({'score': row['scores']})
+
+# write new json
+with open('static/data/map.geojson', 'w') as outfile:
+    json.dump(data, outfile)
