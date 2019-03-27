47440d082cef2c3b0689cba39dbe0a6fa698fa18
Xuyang Han <43256411+xuyangHan@users.noreply.github.com>
2019-03-27 15:59:37 -0400

    Update dashboard.js

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -13,28 +13,6 @@ let yorkU_data = {
 
 let dashboard_data = [yorkU_data];
 
-let layout = {
-        autosize: true,
-        height:600,
-        hovermode: 'closest',
-        margin: {
-            r: 20,
-            t: 40,
-            b: 20,
-            l: 20,
-            pad: 0
-        },
-        showlegend: true,
-        mapbox: {
-            bearing: 0,
-            center: {
-                lat: 43.7735,
-                lon: -79.5019
-            },
-            pitch: 0,
-            zoom: 12
-        },
-    };
 
 Plotly.d3.csv('static/data/example.CSV', function (err, rows) {
     function unpack(rows, key) {
@@ -93,7 +71,7 @@ Plotly.d3.csv('static/data/fitness.CSV', function (err, rows) {
             name: 'fitness',
             marker: {
                 color: unpack(rows, 'rating'),
-                colorscale: 'Jet',
+                colorscale: 'Reds',
                 cmin: 0,
                 cmax: 5,
                 reversescale: false,
@@ -137,44 +115,82 @@ Plotly.d3.csv('static/data/supermarkets.CSV', function (err, rows) {
 });
 
 
+Plotly.d3.json('static/data/map.geojson', function (torontojson) {
+    Plotly.d3.csv('static/data/restaurants.CSV', function (err, rows) {
+        function unpack(rows, key) {
+            return rows.map(function (row) {
+                return row[key];
+            });
+        }
 
+        let rest_data =
+            {
+                type: 'scattermapbox',
+                mode: 'markers',
+                lon: unpack(rows, 'long'),
+                lat: unpack(rows, 'lat'),
+                text: unpack(rows, 'name'),
+                name: 'restaurants',
+                marker: {
+                    color: unpack(rows, 'rating'),
+                    colorscale: 'Reds',
+                    cmin: 0,
+                    cmax: 5,
+                    reversescale: false,
+                    size: 10,
+                    opacity: 0.9,
+                }
+            };
 
+        dashboard_data.push(rest_data);
 
-Plotly.d3.csv('static/data/restaurants.CSV', function (err, rows) {
-    function unpack(rows, key) {
-        return rows.map(function (row) {
-            return row[key];
-        });
-    }
+        let data_layers = [];
+        for (let feature_num = 0; feature_num < torontojson["features"].length; feature_num++) {
+            let source = torontojson["features"][feature_num];
+            let r_value = 255 - source["commute_time"] * 250 / 1500;
+            let g_value = 255 - source["commute_time"] * 125 / 1500;
+            let b_value = 255 - source["commute_time"] * 250 / 1500;
+            let color = 'rgba(' + r_value + ', ' + g_value + ', ' + b_value + ', 0.5)';
+            data_layers.push(
+                {
+                    sourcetype: 'geojson',
+                    source: source,
+                    type: 'fill',
+                    color: color,
+                }
+            );
+        }
 
-    let rest_data =
-        {
-            type: 'scattermapbox',
-            mode: 'markers',
-            lon: unpack(rows, 'long'),
-            lat: unpack(rows, 'lat'),
-            text: unpack(rows, 'name'),
-            name: 'restaurants',
-            marker: {
-                color: unpack(rows, 'rating'),
-                colorscale: 'Reds',
-                cmin: 0,
-                cmax: 5,
-                reversescale: false,
-                size: 10,
-                opacity: 0.9,
+        let layout = {
+            autosize: true,
+            title: "Toronto",
+            height: 700,
+            showlegend: true,
+            hovermode: 'closest',
+            margin: {
+                r: 10,
+                t: 10,
+                b: 10,
+                l: 10,
+                pad: 0
+            },
+            mapbox: {
+                center: {
+                    lat: 43.7735,
+                    lon: -79.5019
+                },
+                // style: 'light',
+                zoom: 12,
+                layers: data_layers,
             }
         };
 
-    dashboard_data.push(rest_data);
 
-    Plotly.plot('dashboardDiv', dashboard_data, layout, {
-        mapboxAccessToken: 'pk.eyJ1IjoiamF5a2Fyb255b3JrIiwiYSI6ImNqa2JjZzNkeTA5ZGkzcG55OXhmcnZxMTIifQ.XotoTIdsT-bYoQpodyW3xg'
+        Plotly.plot('dashboardDiv', dashboard_data, layout, {
+            mapboxAccessToken: 'pk.eyJ1IjoiamF5a2Fyb255b3JrIiwiYSI6ImNqa2JjZzNkeTA5ZGkzcG55OXhmcnZxMTIifQ.XotoTIdsT-bYoQpodyW3xg'
+        });
+
     });
 
+
 });
-
-
-
-
-
