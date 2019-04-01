96a71d8cbeb3391d7832f405b0f4d6c0adae6475
Xuyang Han <43256411+xuyangHan@users.noreply.github.com>
2019-04-01 13:34:31 -0400

    Update york.js

--- static/js/york.js
+++ static/js/york.js
@@ -11,9 +11,49 @@ let yorkU_data = {
     text: ['YorkU']
 };
 
-let dashboard_data = [yorkU_data];
+
 // Plotly.d3.json('static/data/map.geojson', function (torontojson) {
-    Plotly.d3.json('static/data/york.geojson', function (yorkjson) {
+
+    Plotly.d3.csv('static/data/final.CSV', function (err, rows) {
+        function unpack(rows, key) {
+            return rows.map(function (row) {
+                return row[key];
+            });
+        }
+
+        scl = [
+            [0, 'rgb(0, 25, 255)'],
+            [0.2, 'rgb(0, 152, 255)'],
+            [0.4, 'rgb(44, 255, 150)'],
+            [0.5, 'rgb(151, 255, 0)'],
+            [0.6, 'rgb(255, 234, 0)'],
+            [0.8, 'rgb(255, 111, 0)'],
+            [1, 'rgb(255, 0, 0)']];
+
+        let data =
+            {
+                type: 'scattermapbox',
+                mode: 'markers',
+                lon: unpack(rows, 'cent_long'),
+                lat: unpack(rows, 'cent_lat'),
+                text: unpack(rows, 'name'),
+                name: 'score',
+                marker: {
+                    color: unpack(rows, 'scores'),
+                    colorscale: scl,
+                    cmin: 0,
+                    cmax: 1,
+                    reversescale: false,
+                    size: 10,
+                    opacity: 0.9,
+                    colorbar: {
+                        thickness: 10,
+                        titleside: 'right',
+                        outlinecolor: 'rgba(68,68,68,0)',
+                    }
+                }
+            };
+
 
         let layout = {
             autosize: true,
@@ -35,26 +75,13 @@ let dashboard_data = [yorkU_data];
                 },
                 // style: 'light',
                 zoom: 12,
-                layers: [
-                    {
-                        sourcetype: 'geojson',
-                        source: yorkjson,
-                        type: 'fill',
-                        color: 'rgba(163,22,19,0.8)'
-                    },
-                    {
-                        sourcetype: 'geojson',
-                        source: torontojson,
-                        type: 'fill',
-                        color: 'rgba(255,255,255,0.3)'
-                    },
-                ]
             }
         };
 
-
+        let dashboard_data = [yorkU_data, data];
         Plotly.plot('dashboardDiv', dashboard_data, layout, {
             mapboxAccessToken: 'pk.eyJ1IjoiamF5a2Fyb255b3JrIiwiYSI6ImNqa2JjZzNkeTA5ZGkzcG55OXhmcnZxMTIifQ.XotoTIdsT-bYoQpodyW3xg'
         });
     });
+
 // });
